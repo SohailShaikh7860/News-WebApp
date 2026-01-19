3f03989137296730e50dd4bf64d948f17d215033
SohailShaikh7860 <sohailshaikh6417@gmail.com>
2026-01-19 22:51:51 +0530

    Implemented Navbar and button from daisuy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar.jsx'
+import News from './page/News.jsx'
+import Category from './components/Category.jsx'
 
 function App() {
 
   return (
     <>
-      
+      <Navbar />
+      <Category />
+      <News />
     </>
   )
 }
---
+++ src/components/Category.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Wrapper from './Wrapper';
+
+const Category = () => {
+  return (
+    <Wrapper>
+    <div>
+      <button className="btn btn-primary">Primary</button>
+    </div>
+    </Wrapper>
+  )
+}
+
+export default Category;
